b71aa69d7ce2114706830ac656cf93c1a6d4a425
Anton Babenko <anton@antonbabenko.com>
2018-09-22 13:36:40 +0200

    Updated small example

--- examples/small-terraform/README.md
+++ examples/small-terraform/README.md
@@ -7,5 +7,10 @@ Everything is simple and a good start for proof of concepts, hobby projects and 
 ## Features
 
 This code illustrates how it is possible to:
+
 1. Create new VPC and Internet Gateway
-1. Specify existing VPC and attach Internet Gateway to is 
+1. Specify existing VPC and attach Internet Gateway to it 
+
+## See also
+
+Check [Terraform Best Practices](https://www.terraform-best-practices/) for more information about naming, code style and tips for most common problems.
--- examples/small-terraform/main.tf
+++ examples/small-terraform/main.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-terraform {
-  backend "s3" {
-    key = "small-terraform/terraform.tfstate"
-    # ...
-  }
-}
-
 locals {
   create_vpc = "${var.vpc_id == "" ? 1 : 0}"
 }
@@ -35,25 +28,4 @@ resource "aws_internet_gateway" "this" {
   count = "${local.create_vpc}"
 
   vpc_id = "${aws_vpc.this.id}"
-}
-
-module "iam_user" {
-  source = "terraform-aws-modules/iam/aws"
-
-  name          = "anton"
-  force_destroy = true
-
-  pgp_key = "keybase:me"
-
-  password_reset_required = false
-}
-
-resource "aws_iam_group_membership" "team" {
-  name = "tf-testing-group-membership"
-
-  group = "my-group"
-
-  users = [
-    "${module.iam_user.this_iam_user_name}",
-  ]
 }
